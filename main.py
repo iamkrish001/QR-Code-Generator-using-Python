799b45b49cef5b9732fc8803696d4ae9790efc4f
Krish Bhurtel <bhurtelkrish123@gmail.com>
2024-03-27 20:04:48 +0545

    added qr code of website

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from pyqrcode import QRCode
   
 # String which represent the QR code 
 s = "https://github.com/iamkrish001"   
+a = "https://www.krishbhurtel.com.np"
 # Generate QR code 
 url = pyqrcode.create(s) 
+url = pyqrcode.create(a) 
   
 # Create and save the png file naming "myqr.png" 
-url.png("mygithub.png", scale = 12)
+url.png("mygithub.png", scale = 10)
+url.png("mywebsite.png", scale = 10)
