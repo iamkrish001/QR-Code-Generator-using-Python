4b6360b987dc1424a501714125a8c742093d0776
Krish Bhurtel <bhurtelkrish123@gmail.com>
2024-03-30 16:18:38 +0545

    added qr of social media handles

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ from pyqrcode import QRCode
 # String which represent the QR code 
 s = "https://github.com/iamkrish001"   
 a = "https://www.krishbhurtel.com.np"
+b = "https://www.linkedin.com/in/krish-bhurtel-894946233/"
+d = "https://www.instagram.com/b_krishhh/"
 # Generate QR code 
 url = pyqrcode.create(s) 
 url = pyqrcode.create(a) 
+url = pyqrcode.create(b) 
+url = pyqrcode.create(d) 
   
 # Create and save the png file naming "myqr.png" 
-url.png("mygithub.png", scale = 10)
-url.png("mywebsite.png", scale = 10)
+url.png("mygithub.png", scale = 5)
+url.png("mywebsite.png", scale = 5)
+url.png("mylinkedin.png", scale = 5)
+url.png("myinstagram.png", scale = 5)
